a915c55501676dedeef89a48a5b4022f9591ebde
Arne De Smedt <arnedesmdt@gmail.com>
2020-09-21 16:31:38 +0200

    typo and unused code

--- resources/dev/elastic-openapi-codegen.sh
+++ resources/dev/elastic-openapi-codegen.sh
@@ -11,17 +11,14 @@ case "$(uname -s)" in
   MSYS*|MINGW*) use_winpty=1;;
   Linux) generatordir=$(dirname "$(readlink -f "$0" || echo "$argv0")");;
 esac
-echo $generatordir
 
 generatordir=$(cd $(dirname $argv0) > /dev/null && cd $generatordir > /dev/null && pwd)
-generaroimage=elastic/elastic-openapi-codegen-php
+generatorimage=elastic/elastic-openapi-codegen-php
 rootdir=`cd $(dirname $argv0)/../..; pwd`
-fixerimage="php:7.4.5-zts-buster"
-echo $rootdir
 
-cd ${generatordir} && docker build --target runner -t ${generaroimage} elastic-openapi-codegen-php
+cd ${generatordir} && docker build --target runner -t ${generatorimage} elastic-openapi-codegen-php
 
-docker run --rm -v ${rootdir}:/local ${generaroimage} generate -g elastic-php-client \
+docker run --rm -v ${rootdir}:/local ${generatorimage} generate -g elastic-php-client \
                                                                -i /local/resources/api/api-spec.yml \
                                                                -o /local/ \
                                                                -c /local/resources/api/config.json \
