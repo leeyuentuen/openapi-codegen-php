bc0640ccafd076a6599521b11f3cc621943edd00
Arne De Smedt <arnedesmdt@gmail.com>
2020-10-08 17:01:31 +0200

    forgot map to name

--- resources/dev/elastic-openapi-codegen-php/src/main/java/org/elasticsearch/codegen/ElasticClientPhpGenerator.java
+++ resources/dev/elastic-openapi-codegen-php/src/main/java/org/elasticsearch/codegen/ElasticClientPhpGenerator.java
@@ -156,6 +156,7 @@ public class ElasticClientPhpGenerator extends PhpClientCodegen implements Codeg
                     List<String> requiredParameters = queryParameters
                         .stream()
                         .filter(parameter -> !(parameter.getRequired() == null || parameter.getRequired() == false))
+                        .map(parameter -> parameter.getName())
                         // see above
 //                         .map(parameter -> org.openapitools.codegen.utils.StringUtils.camelize(parameter.getName(), true))
                         .collect(Collectors.toList());
