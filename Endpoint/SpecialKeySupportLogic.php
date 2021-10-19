2a6b359fe316a77a2a0b47006cbdf9d0bd778fb9
Arne De Smedt <arnedesmdt@gmail.com>
2021-10-19 14:22:35 +0200

    add special key support

---
+++ Endpoint/SpecialKeySupportLogic.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Elastic\OpenApi\Codegen\Endpoint;
+
+trait SpecialKeySupportLogic
+{
+    /**
+     * @param array<string, string> $map
+     */
+    private function convertByMap(string $key, array $map): string
+    {
+        if (! array_key_exists($key, $map)) {
+            return $key;
+        }
+
+        return $map[$key];
+    }
+}
